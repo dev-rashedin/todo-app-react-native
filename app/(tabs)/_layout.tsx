9b3b5942da8f50c63c76c82be6dc4d6722121bf7
Rashedin-063 <rashedinislam.06@gmail.com>
2025-12-27 20:49:27 +0600

    tab navigator updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,42 @@
 import { Tabs } from 'expo-router'
-import { View, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{}}>
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: 'green',
+      tabBarStyle: {
+        backgroundColor: "white",
+        borderTopWidth: 1,
+        borderTopColor: "gray",
+        height: 90,
+        paddingBottom: 30,
+        paddingTop: 15
+      },
+      tabBarLabelStyle: {
+        fontSize: 14,
+        fontWeight: "600"
+      },
+      headerShown: false
+    }}>
       <Tabs.Screen
-        name="index"
+        name='index'
         options={{
-          title: "Todos"
+          title: 'Todos',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='flash-outline' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='settings' size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
-  )
+  );
 }
 export default TabsLayout
