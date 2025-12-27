b8dc63682e8a2715e2997e84264933242e072ebf
Rashedin-063 <rashedinislam.06@gmail.com>
2025-12-27 19:16:04 +0600

    expo navigation explored

---
+++ app/about.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+const About = () => {
+  return (
+    <View>
+      <Text>This is the about screen</Text>
+    </View>
+  )
+}
+export default About
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>
+        Edit app/index.tsx to edit this screen{' '}
+      </Text>
+      <Text> Hi There</Text>
+      <Link href="/about">Visit about screen</Link>
     </View>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    gap: 10,
+  },
+  content: {
+    fontSize: 40
+  }
+});
